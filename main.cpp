4bafc93d7979ec33fed55051309d3af6e21f45f5
brewerj3 <joshuabrewer784@gmail.com>
2022-03-08 18:24:05 -1000

    Added main.cpp header

---
+++ main.cpp
@@ -0,0 +1,17 @@
+///////////////////////////////////////////////////////////////////////////////
+/// University of Hawaii, College of Engineering
+/// @brief ee205_lab08c_all_creatures_great_and_small - EE 205 - Spr 2022
+///
+/// @file main.cpp
+/// @version 1.0
+///
+/// @author Joshua Brewer <brewerj3@hawaii.edu>
+/// @date 08_Mar_2022
+///////////////////////////////////////////////////////////////////////////////
+
+#include "animal.h"
+
+int main() {
+    Animal myFirstAnimal = Animal( "Felis Catus");
+
+}
